63698af32b8d6eeecbf1a45770a6f9a779eec4c8
Masaki Takahashi <masakitk@gmail.com>
2012-10-31 06:48:08 +0900

    writing test for Napoleon.tellTheAdjutant

--- src/napoleon/model/player/NapoleonTest.java
+++ src/napoleon/model/player/NapoleonTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import napoleon.model.card.Card;
 import napoleon.model.card.Suit;
 import napoleon.model.rule.Declaration;
+import napoleon.model.rule.GameContext;
 
 import org.hamcrest.core.IsEqual;
 import org.junit.Test;
@@ -24,5 +25,48 @@ public class NapoleonTest {
 		napoleon.takeCard(Card.New(Suit.Club, 13));
 		napoleon.takeCard(Card.New(Suit.Club, 11));
 		assertThat(napoleon.SomeTrumpCard(declaration), equalTo(Card.New(Suit.Club, 12)));
+	}
+	
+	@Test
+	public void T02_副官指定マイティ() {
+		Player player = Player.New("hoge");
+		player.takeCard(Card.New(Suit.Club, 1));
+		Napoleon napoleon = Napoleon.New(player);
+		
+		Declaration declaration = Declaration.New(Suit.Club, 14);
+		assertThat(napoleon.tellTheAdjutant(declaration), equalTo(Card.Mighty));
+	}
+	
+	@Test
+	public void T03_副官指定正J() {
+		Player player = Player.New("hoge");
+		player.takeCard(Card.Mighty);
+		Napoleon napoleon = Napoleon.New(player);
+		
+		Declaration declaration = Declaration.New(Suit.Club, 14);
+		assertThat(napoleon.tellTheAdjutant(declaration), equalTo(GameContext.getRightBower(Suit.Club)));
+	}
+
+	@Test
+	public void T04_副官指定裏J() {
+		Player player = Player.New("hoge");
+		player.takeCard(Card.Mighty);
+		player.takeCard(GameContext.getRightBower(Suit.Club));
+		Napoleon napoleon = Napoleon.New(player);
+		
+		Declaration declaration = Declaration.New(Suit.Club, 14);
+		assertThat(napoleon.tellTheAdjutant(declaration), equalTo(GameContext.getLeftBower(Suit.Club)));
+	}
+
+	@Test
+	public void T05_副官指定よろめき() {
+		Player player = Player.New("hoge");
+		player.takeCard(Card.Mighty);
+		player.takeCard(GameContext.getRightBower(Suit.Club));
+		player.takeCard(GameContext.getLeftBower(Suit.Club));
+		Napoleon napoleon = Napoleon.New(player);
+		
+		Declaration declaration = Declaration.New(Suit.Club, 14);
+		assertThat(napoleon.tellTheAdjutant(declaration), equalTo(Card.Yoromeki));
 	}
 }
